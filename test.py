ea0ce5103a767dc2983da15db1c6eb59e8185d22
divinejoshua <divineuche399@gmail.com>
2024-07-12 21:48:55 +0100

    Updated gpt

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 from decouple import config
 import os
+
+# llama index requires the os.environ variable called OPENAI_API_KEY:
 os.environ['OPENAI_API_KEY'] = config('OPENAI_API_KEY')
 
+from llama_index.llms.openai import OpenAI
 
-print(os.environ['OPENAI_API_KEY'] )
+llm = OpenAI(model="gpt-3.5-turbo", api_key="BAD_KEY")
+resp = OpenAI().complete("Paul Graham is ")
+print(resp)
